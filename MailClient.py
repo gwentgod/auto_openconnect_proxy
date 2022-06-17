645fa70dfb32fe88626404565064b80fe34a31a2
gwentgod <ziyaozhu@connect.hku.hk>
2022-06-17 18:58:47 +0800

    chores

--- MailClient.py
+++ MailClient.py
@@ -30,7 +30,7 @@ class MailClient:
                 mail = message_from_bytes(mail_data[0][1])
                 return mail
             else:
-                logging.info('No mail found in selected mail list')
+                logging.debug('No mail found in selected mail list')
         except Exception as e:
             logging.error(e)
 
@@ -46,7 +46,7 @@ class MailClient:
                 continue
 
             sent_time = datetime.strptime(mail['Date'].split(', ')[-1], "%d %b %Y %H:%M:%S %z")
-            logging.info(f'Last mail was sent at {sent_time.isoformat()}')
+            logging.debug(f'Last mail was sent at {sent_time.isoformat()}')
             deltat = self.initial_time - sent_time
             if sent_time > self.initial_time and deltat < timedelta(minutes=5):
                 token = mail['Subject'].split()[-1]
